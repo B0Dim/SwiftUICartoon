014c1a6a59d82f603f575cfde19b5468210aadf6
B0Dim <bodim@me.com>
2022-02-17 10:23:15 +0300

    Initial Commit

---
+++ SwiftUICartoon/SwiftUICartoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICartoonApp.swift
+//  SwiftUICartoon
+//
+//  Created by  BoDim on 17.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICartoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
